f79dcb4962c0fe02ebf6db4497ce6835323811eb
Felix Suen <felixsuen@mbpserver.local>
2023-08-24 14:15:17 -0700

    fixed some bugs

--- wordz.py
+++ wordz.py
@@ -1,60 +1,80 @@
 import eng_to_ipa as ipa
-from random import randint
+import random
 import json
 import re
 
 f = open('./sound.json')
 sound = json.load(f)
+replacement_ratio = 0.6
 
-def ipa_to_eng(w):
+def assign_probabilities(input_list):
+    total_items = len(input_list)
+    probabilities = [1 / (i + 1) for i in range(total_items)]
+    return probabilities
+
+def ipa_to_eng(w, actual_word):
     w = w.replace('ˌ', '')
     w = w.replace("ˈ", "")
-    char = list(w)
-    length = len(char)
+    chars = list(w)
+    length = len(chars)
     index = 0
     regroup = []
     while index < length:
         phonetic = ""
-        if char[index] == 'e' and index+1 < length and char[index+1] == 'ɪ':
+        if chars[index] == 'e' and index+1 < length and chars[index+1] == 'ɪ':
             phonetic = 'eɪ'
             index += 1
-        elif char[index] == 'a' and index+1 < length and char[index+1] == 'ʊ':
+        elif chars[index] == 'a' and index+1 < length and chars[index+1] == 'ʊ':
             phonetic = 'aʊ'
             index += 1
-        elif char[index] == 'a' and index+1 < length and char[index+1] == 'ɪ':
+        elif chars[index] == 'a' and index+1 < length and chars[index+1] == 'ɪ':
             phonetic = 'aɪ'
             index += 1
-        elif char[index] == 'ə' and index+1 < length and char[index+1] == 'r':
+        elif chars[index] == 'ə' and index+1 < length and chars[index+1] == 'r':
             phonetic = 'ər'
             index += 1
-        elif char[index] == 'o' and index+1 < length and char[index+1] == 'ʊ':
+        elif chars[index] == 'o' and index+1 < length and chars[index+1] == 'ʊ':
             phonetic = 'oʊ'
             index += 1
-        elif char[index] == 'ɔ' and index+1 < length and char[index+1] == 'ɪ':
+        elif chars[index] == 'ɔ' and index+1 < length and chars[index+1] == 'ɪ':
             phonetic = 'ɔɪ'
             index += 1
         else:
-            phonetic = char[index]
+            phonetic = chars[index]
         
         index += 1
         if phonetic in sound:
-            regroup.append(sound[phonetic][randint(0, len(sound[phonetic])-1)])
+            weighted_probability = assign_probabilities(sound[phonetic])
+            selected = random.choices(sound[phonetic], weights=weighted_probability, k=1)[0]
+            regroup.append(selected)
+        elif phonetic == 'ɪ':
+            for char in actual_word:
+                if char == 'i':
+                    regroup.append('i')
+                    break
+                elif char == 'o':
+                    regroup.append('o')
+                    break
         else:
             regroup.append(phonetic)
     
     return "".join(regroup)
 
-while True:
-    print()
-    line = input("Enter a sentence: \n")
-    line = re.sub(r'[^\w\s]', '', line)
-    arr = line.split(" ")
-    output = []
+try:
+    while True:
+        print()
+        line = input("Enter a sentence: \n")
+        line = re.sub(r'[^\w\s]', '', line)
+        arr = line.split(" ")
+        output = []
 
-    for word in arr:
-        wordz = ipa.convert(word)
-        output_word = ipa_to_eng(wordz)
-        output.append(output_word)
+        for word in arr:
+            wordz = ipa.convert(word)
+            output_word = ipa_to_eng(wordz, word)
+            output.append(output_word) if random.random() <= replacement_ratio else output.append(word)
 
+        print()
+        print(" ".join(output))
+except KeyboardInterrupt:
     print()
-    print(" ".join(output))
+    print("cya")
